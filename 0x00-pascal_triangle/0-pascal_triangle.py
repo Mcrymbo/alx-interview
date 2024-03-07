ca94defd6906883bc426e8e4a6bd399b113e4b6a
mcrymbo <croco2023@gmail.com>
2024-03-07 20:40:03 +0300

    fix pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 function for developing pascal's triangle
 """
 
+
 def pascal_triangle(n):
     """ function for pascal triagle """
     if n <= 0:
@@ -15,7 +16,8 @@ def pascal_triangle(n):
                 if (col == 0):
                     pascal[row].append(1)
                 else:
-                    pascal[row].append(pascal[row - 1][col] + pascal[row - 1][col - 1])
+                    pascal[row].append(pascal[row - 1][col] +
+                                       pascal[row - 1][col - 1])
             elif (col == row):
                 pascal[row].append(1)
 
