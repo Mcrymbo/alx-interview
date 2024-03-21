b3ae52ba2ce96069f31f9b3e1295d7d6538e540e
mcrymbo <croco2023@gmail.com>
2024-03-21 20:08:21 +0300

    min number of operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,13 +5,14 @@ Module that calculates the fewest number of operations
 
 
 def minOperations(n):
-    if n == 1:
+    """
+    returns min operations to get n Hs
+    """
+    operations = 0
+    if n <= 1:
         return 0
-
-    dp = [0] * (n + 1)
     for i in range(2, n + 1):
-        dp[i] = float('inf')
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + (i // j))
-    return dp[n] if dp[n] != float('inf') else 0
+        while n % i == 0:
+            n = n / i
+            operations += i
+    return operations
