e09a2c23d6919ea3f7d6a086a84d80e8910adb7c
mcrymbo <croco2023@gmail.com>
2024-04-03 16:41:31 +0300

    utf-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,19 @@
+#!/usr/python3
+"""
+module for determining if dataset is a valid utf-8 encording
+"""
+
+
+def validUTF8(data):
+    """
+    function that checks if a dataset is a valid utf-8 encorded
+    """
+    try:
+        for num in data:
+            if num < 0 or num > 255:
+                return False
+        byte_data = bytes(data)
+        byte_data.decode('utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
